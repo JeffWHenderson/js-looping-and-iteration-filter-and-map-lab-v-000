02041afcc292ee553c0c9edbf38cd30280801472
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-21 21:38:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,10 @@ function exactMatch(drivers, object) {
     }
   })
 }
+
+function exactMatchToList(drivers, object) {
+  return exactMatch(drivers, object)
+  .map(function(driver){
+    return driver.name
+  })
+}
